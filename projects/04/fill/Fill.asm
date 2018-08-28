fc8fb18cef701982c99099f62d118e7d32961fce
A217491 <A217491@SEGOTW10249883>
2018-08-28 08:46:26 +0200

    Fill screen assembly program

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,64 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+@8192
+D=A
+@n
+M=D // n = 8192
+
+(OUTERLOOP)
+  @SCREEN
+  D=A
+  @addr
+  M=D // addr = 16384 (base address of screen memory map)
+  
+  @i
+  M=0 // i = 0
+  
+  @KBD
+  D=M
+  @BLACKLOOP
+  D;JGT // if RAM[KBD] > 0 goto BLACKLOOP
+  
+  (WHITELOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @OUTERLOOP
+    D;JEQ // if i == n goto OUTERLOOP
+    
+    @addr
+    A=M
+    M=0 // RAM[addr] = 0
+    
+    @addr
+    M=M+1 // addr = addr + 1
+    
+    @i
+    M=M+1 // i = i + 1
+    
+    @WHITELOOP
+    0;JMP
+    
+  (BLACKLOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @OUTERLOOP
+    D;JEQ // if i == n goto OUTERLOOP
+    
+    @addr
+    A=M
+    M=-1 // RAM[addr] = 111111111111111
+    
+    @addr
+    M=M+1 // addr = addr + 1
+    
+    @i
+    M=M+1 // i = i + 1
+    
+    @BLACKLOOP
+    0;JMP
+  
