a43a6638959f901c06e2ecd31f4eeba94a1562ee
A217491 <A217491@SEGOTW10249883>
2018-09-21 16:19:14 +0200

    more assembly programs

---
+++ projects/06/Screen.asm
@@ -0,0 +1,27 @@
+  @R0
+  D=M
+  @END
+  D;JLE
+  @counter
+  M=D
+  @SCREEN
+  D=A
+  @x
+  M=D
+(LOOP)
+  @x
+  A=M
+  M=-1
+  @x
+  D=M
+  @32
+  D=D+A
+  @x
+  M=D
+  @counter
+  MD=M-1
+  @LOOP
+  D;JGT
+(END)
+  @END
+  0;JMP
