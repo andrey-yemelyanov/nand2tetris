01e5934de8d40b94625b287e0c752c3448b6f426
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2018-10-01 21:41:28 +0200

    arithm ops

--- projects/07/VMTranslator/arithmetic_tests.py
+++ projects/07/VMTranslator/arithmetic_tests.py
@@ -2,25 +2,82 @@ import unittest
 import arithmetic
 from constants import *
 import textwrap
+import re
 
 #python -m unittest discover -p "*_tests.py"
 class ArithmeticTests(unittest.TestCase):
 
     def test_add(self):
-        expected = "add"
+        expected = textwrap.dedent(""" 
+            // add
+            @SP
+            M=M-1 //SP--
+            A=M
+            D=M
+            @SP
+            M=M-1 //SP--
+            A=M
+            M=D+M //M=x+y
+            @SP
+            M=M+1 //SP++
+            """).strip()
         self.assertEqual(expected, arithmetic.translate_command(ADD))
 
     def test_sub(self):
-        expected = "sub"
+        expected = textwrap.dedent(""" 
+            // sub
+            @SP
+            M=M-1 //SP--
+            A=M
+            D=M
+            @SP
+            M=M-1 //SP--
+            A=M
+            M=M-D //M=x-y
+            @SP
+            M=M+1 //SP++
+        """).strip()
         self.assertEqual(expected, arithmetic.translate_command(SUB))
 
     def test_neg(self):
-        expected = "neg"
+        expected = textwrap.dedent(""" 
+            // neg
+            @SP
+            M=M-1 //SP--
+            A=M
+            M=-M //negate
+            @SP
+            M=M+1 //SP++
+        """).strip()
         self.assertEqual(expected, arithmetic.translate_command(NEG))
 
     def test_eq(self):
-        expected = "eq"
-        self.assertEqual(expected, arithmetic.translate_command(EQ))
+        expected = textwrap.dedent(r""" 
+            // eq
+            @SP
+            M=M-1
+            A=M
+            D=M
+            @SP
+            M=M-1
+            A=M
+            D=D-M
+            @EQUAL_\S+
+            D;JEQ
+            @SP
+            A=M
+            M=0
+            @END_\S+
+            0;JMP
+            (EQUAL_\S+)
+            @SP
+            A=M
+            M=-1
+            (END_\S+)
+            @SP
+            M=M+1
+        """).strip()
+        self.assertIsNotNone(re.match(arithmetic.translate_command(EQ), expected))
 
     def test_gt(self):
         expected = "gt"
@@ -31,15 +88,47 @@ class ArithmeticTests(unittest.TestCase):
         self.assertEqual(expected, arithmetic.translate_command(LT))
 
     def test_and(self):
-        expected = "and"
+        expected = textwrap.dedent(""" 
+            // and
+            @SP
+            M=M-1
+            A=M
+            D=M
+            @SP
+            M=M-1
+            A=M
+            M=M&D
+            @SP
+            M=M+1
+        """).strip()
         self.assertEqual(expected, arithmetic.translate_command(AND))
 
     def test_or(self):
-        expected = "or"
+        expected = textwrap.dedent(""" 
+            // or
+            @SP
+            M=M-1
+            A=M
+            D=M
+            @SP
+            M=M-1
+            A=M
+            M=M|D
+            @SP
+            M=M+1
+        """).strip()
         self.assertEqual(expected, arithmetic.translate_command(OR))
 
     def test_not(self):
-        expected = "not"
+        expected = textwrap.dedent(""" 
+            // not
+            @SP
+            M=M-1
+            A=M
+            M=!M
+            @SP
+            M=M+1
+            """).strip()
         self.assertEqual(expected, arithmetic.translate_command(NOT))
 
 if __name__=="__main__":
